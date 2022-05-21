8154d1a22ee052b30115856a118cb48035e7db2e
dvvinokurov <dmitvitvin@gmail.com>
2022-05-21 12:26:25 +0300

    add task63

---
+++ task63/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintNums(int maxNum, int currentNum = 1)
+{
+    if(currentNum <= maxNum)
+    {
+        string symbol = ", ";
+        if(currentNum == maxNum)
+        {
+            symbol = ".";
+        }
+        Console.Write(currentNum + symbol);
+        currentNum++;
+        PrintNums(maxNum, currentNum);
+    }
+}
+
+PrintNums(n);
